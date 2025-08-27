79533fbfa7a2e5ba1c63b71cc11795ec5fc9f488
Terry <talb5624@students.vsu.edu>
2025-08-27 16:22:24 -0400

    feat: add min-length>=8 as additional password criterion

--- PasswordLib/PasswordCheck.cs
+++ PasswordLib/PasswordCheck.cs
@@ -15,8 +15,8 @@ public static class PasswordCheck
         bool hasUpper  = password.Any(char.IsUpper);
         bool hasLower  = password.Any(char.IsLower);
         bool hasDigit  = password.Any(char.IsDigit);
+        bool hasMinLen8 = password.Length >= 8;
 
-        // Count punctuation/symbols as "symbol", exclude whitespace
         bool hasSymbol = password.Any(ch => char.IsPunctuation(ch) || char.IsSymbol(ch));
 
         int met = 0;
@@ -24,14 +24,16 @@ public static class PasswordCheck
         if (hasLower)  met++;
         if (hasDigit)  met++;
         if (hasSymbol) met++;
+        if (hasMinLen8) met++;  // NEW criterion
+
 
         return met switch
         {
-            0       => Ineligible,
-            1       => "WEAK",
-            2 or 3  => "MEDIUM",
-            4       => "STRONG",
-            _       => Ineligible
+            0 => Ineligible,
+            1 => "WEAK",
+            2 or 3 => "MEDIUM",
+            4 => "STRONG",
+            _ => Ineligible
         };
     }
 }
